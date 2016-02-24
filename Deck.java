b15ac88d481d004365cbf1e909a306b545a1eab4
Jesse Sargent <mountaneer12@gmail.com>
2016-02-24 15:05:15 -0800

    Declared deck array in class

--- Deck.java
+++ Deck.java
@@ -1,14 +1,16 @@
 
 public class Deck {
-
-	private Card[] deck;
 	
-	public Deck() {
-		
+	private Card[] deck;
+
+	//Constructor to create a standard 52 card deck
+	public Deck() 
+	{
 		deck = new Card[52];
 		int count = 0;
 		
-		for(int suit = 0; suit<3; suit++) 
+		//Fills the deck with cards
+		for(int suit = 0; suit<=3; suit++) 
 		{
 			for(int value = 1; value <= 13; value++) 
 			{
@@ -17,8 +19,5 @@ public class Deck {
 			}
 		}	
 	}
-	
-	
-	
 	
 }
