10ab350352032ec6d66b0b1f1ac0874d4f3b0342
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-26 23:31:18 -0800

    Create Computer.java
    Note to self: remove comment before submission or we fail.

---
+++ Computer.java
@@ -0,0 +1,15 @@
+
+public class Computer extends Player{
+	void requestCard(){
+		/*I'm pretty sure this is the only method that needs to be added
+		 * or overriden for this class. I think that the best way to set 
+		 * this up is to use a random number generator to select a random
+		 * card from the user's hand. If we REALLY wanted to, we could use
+		 * some basic AI and have the computer notice when a player draws a
+		 * card the the computer has in it's hand, but I really just want to 
+		 * get this project done with. Like I would be so happy if the words 
+		 * "personal software process" were never mentioned again.
+		 */
+	}
+
+}
