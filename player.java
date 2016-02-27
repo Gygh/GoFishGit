34ec6267d722541b0f52602cb63ab61a3b0f4972
Andrew Fiorentino <fiorentinoandrew@gmail.com>
2016-02-26 23:29:50 -0800

    Update player.java
    Most of this class is finished. I'm not sure how we're going to handle requesting the card and taking the card yet

--- player.java
+++ player.java
@@ -1,1 +1,31 @@
+public class Player {
+	// ----------------------------------------------------------------------------
+	// A virtual representation of a player in a card game.
+	// ----------------------------------------------------------------------------
+	private String name;
 
+	public Player()
+	// Default constructor for player class
+	{
+		Hand playerHand = new Hand();
+	}
+
+	private String getName()
+	// Accessor method for the player's name
+	{
+		return name;
+	}
+
+	private void setName(String theName)
+	// Mutator method for the player's name
+	{
+		name = theName;
+	}
+
+	void requestCard(Card theCard)
+	// Requests a selected card from the opponent.
+	{
+		// not sure how to do this yet. Things are still kind of abstract
+	}
+
+}
